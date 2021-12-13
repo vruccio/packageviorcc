7528f6bcc5e22308f2f73aaab82355c4e0e10b54
violette ruccio <violetteruccio@gmail.com>
2021-12-13 16:08:28 +0100

    challenge 4 ok

--- packageviorcc/lib.py
+++ packageviorcc/lib.py
@@ -1,2 +1,5 @@
 def try_me():
     return "Welcome in my vruccio package!"
+
+def help_from_ta():
+    return "I need help for my challenge!"
--- tests/test_lib.py
+++ tests/test_lib.py
@@ -3,3 +3,8 @@ from mlproject.lib import hello_world
 
 def test_length_of_hello_world():
     assert len(hello_world()) != 0
+
+
+from packageviorcc.lib import help_from_ta
+
+len(test_functions) == len(help_from_ta)
